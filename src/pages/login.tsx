eecada78a5af637997eb6457c4eae8f551039815
tanchohang <tanchohang@gmail.com>
2023-04-14 04:02:16 +0100

    responsive(auth UI): Make Login and Signup page responsive

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-cyan-500">
-      <form className="flex flex-col gap-8 w-[30%]">
-        <div className="bg-red-200 text-red-600 font-semibold p-4 ml-10 rounded-sm">
+    <div className="flex justify-center items-center min-h-screen bg-cyan-500 text-base md:text-lg font-normal md:font-semibold">
+      <form className="flex flex-col gap-8 w-[80%] md:w-[30%]">
+        <div className="bg-red-200 text-red-600 p-4 ml-10 rounded-sm text-xs">
           <p>! Username or Password is wrong</p>
+          type="text"
         </div>
         <label className="flex gap-5  items-center ">
           <User className="stroke-white" />
           <input
-            type="text"
             name="username"
             placeholder="Username"
-            className="text-lg p-3 text-cyan-500 font-semibold focus:outline-cyan-500 rounded-lg w-[100%]"
+            className="text-lg sm:text-base p-3 text-cyan-500  focus:outline-cyan-500 rounded-lg w-[100%]"
           />
         </label>
         <label className="flex gap-5  items-center  w-[100%]">
@@ -23,15 +23,15 @@ const Login = () => {
             type="password"
             name="password"
             placeholder="Password"
-            className="text-lg p-3 text-cyan-500 font-semibold focus:outline-cyan-500 rounded-lg w-[100%]"
+            className="text-lg sm:text-base p-3 text-cyan-500  focus:outline-cyan-500 rounded-lg w-[100%]"
           />
         </label>
         <div className="flex flex-col items-center text-white">
-          <button type="submit" className=" text-lg font-bold hover:opacity-90 shadow-lg bg-white text-cyan-500 w-[100px] h-[50px]">
+          <button type="submit" className=" hover:opacity-90 shadow-lg bg-white text-cyan-500 w-[100px] h-[50px]">
             Login
           </button>
           or{' '}
-          <Link to="/signup" className="text-lg font-semibold underline">
+          <Link to="/signup" className="underline">
             Signup
           </Link>
         </div>
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -2,53 +2,27 @@ import { Link } from 'react-router-dom';
 
 const Signup = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-cyan-500">
-      <form className="flex flex-col gap-8 w-[30%]">
-        <div className="bg-red-200 text-red-600 font-semibold p-4  rounded-sm">
+    <div className="flex justify-center items-center min-h-screen bg-cyan-500 text-base md:text-lg  font-normal md:font-semibold">
+      <form className="flex flex-col gap-8 w-[80%] md:w-[30%]">
+        <div className="bg-red-200 text-red-600 p-4  rounded-sm">
           <p>! Username or Password is wrong</p>
         </div>
 
-        <div className="flex justify-between">
-          <input
-            type="text"
-            name="Firstname"
-            placeholder="Firstname"
-            className="text-lg p-3 text-cyan-500 font-semibold focus:outline-cyan-500 rounded-lg "
-          />
-          <input
-            type="text"
-            name="Lastname"
-            placeholder="Lastname"
-            className="text-lg p-3 text-cyan-500 font-semibold focus:outline-cyan-500 rounded-lg "
-          />
+        <div className="flex md:justify-between w-[100%] gap-3">
+          <input type="text" name="Firstname" placeholder="Firstname" className=" p-3 text-cyan-500  focus:outline-cyan-500 rounded-lg w-[100%] " />
+          <input type="text" name="Lastname" placeholder="Lastname" className=" p-3 text-cyan-500 focus:outline-cyan-500 rounded-lg w-[100%] " />
         </div>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          className="text-lg p-3 text-cyan-500 font-semibold focus:outline-cyan-500 rounded-lg w-[100%]"
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="email"
-          className="text-lg p-3 text-cyan-500 font-semibold focus:outline-cyan-500 rounded-lg w-[100%]"
-        />
+        <input type="text" name="username" placeholder="Username" className=" p-3 text-cyan-500 focus:outline-cyan-500 rounded-lg w-[100%]" />
+        <input type="email" name="email" placeholder="email" className=" p-3 text-cyan-500 focus:outline-cyan-500 rounded-lg w-[100%]" />
 
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          className="text-lg p-3 text-cyan-500 font-semibold focus:outline-cyan-500 rounded-lg w-[100%]"
-        />
+        <input type="password" name="password" placeholder="Password" className=" p-3 text-cyan-500 focus:outline-cyan-500 rounded-lg w-[100%]" />
 
         <div className="flex flex-col items-center justify-center">
-          <button type="submit" className=" text-lg font-bold hover:opacity-90 shadow-lg bg-white text-cyan-500 w-[100px] h-[50px]">
+          <button type="submit" className=" hover:opacity-90 shadow-lg bg-white text-cyan-500 w-[100px] h-[50px]">
             Signup
           </button>
-          or
-          <Link to="/login" className="text-white underline font-semibold text-lg">
-            Login
+          <Link to="/login" className="text-white underline ">
+            or Login
           </Link>
         </div>
       </form>
