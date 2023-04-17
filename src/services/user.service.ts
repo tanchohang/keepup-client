388e5324e7317ec54204109f450f5e2597f0ce39
tanchohang <tanchohang@gmail.com>
2023-04-17 16:14:27 +0100

    feat: scaffold user service to use axios instance

---
+++ src/services/user.service.ts
@@ -0,0 +1,27 @@
+import { User } from '../context/auth.context';
+import { usersEndpoint, keepupApiAxiosInstance } from '../utils/axios';
+
+export const createUser = async (user: User) => {
+  const response = await keepupApiAxiosInstance(usersEndpoint).post('/', user);
+  return response;
+};
+
+export const readAllUser = async () => {
+  const response = await keepupApiAxiosInstance(usersEndpoint).get('/');
+  return response;
+};
+
+export const readUser = async (id: string) => {
+  const response = await keepupApiAxiosInstance(usersEndpoint).get(id);
+  return response;
+};
+
+export const updateUser = async (id: string, name: string) => {
+  const response = await keepupApiAxiosInstance(usersEndpoint).put(id, { name });
+  return response;
+};
+
+export const deleteUser = async (id: string) => {
+  const response = await keepupApiAxiosInstance(usersEndpoint).delete(id);
+  return response;
+};
