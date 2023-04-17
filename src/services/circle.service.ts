2896518f1dfc9b44e529b984a982694684622ca6
tanchohang <tanchohang@gmail.com>
2023-04-17 15:30:03 +0100

    Refactor: Fix error caused from axios instance refactor

--- src/services/circle.service.ts
+++ src/services/circle.service.ts
@@ -1,36 +1,36 @@
-import { circlesEndpoint, keepupApi } from '../utils/axios';
+import { circlesEndpoint, keepupApiAxiosInstance } from '../utils/axios';
 
 export const createCircle = async (uid: string) => {
-  const response = await keepupApi.post(circlesEndpoint, { users: [uid] });
+  const response = await keepupApiAxiosInstance(circlesEndpoint).post('/', { users: [uid] });
   return response;
 };
 
 export const readAllCircle = async () => {
-  const response = await keepupApi.get(circlesEndpoint);
+  const response = await keepupApiAxiosInstance(circlesEndpoint).get('/');
   return response;
 };
 
 export const readCircle = async (id: string) => {
-  const response = await keepupApi.get(circlesEndpoint);
+  const response = await keepupApiAxiosInstance(circlesEndpoint).get(id);
   return response;
 };
 
 export const updateCircle = async (id: string, name: string) => {
-  const response = await keepupApi.put(`${circlesEndpoint}/id`, { name });
+  const response = await keepupApiAxiosInstance(circlesEndpoint).put(id, { name });
   return response;
 };
 
 export const deleteCircle = async (id: string) => {
-  const response = await keepupApi.delete(`${circlesEndpoint}/id`);
+  const response = await keepupApiAxiosInstance(circlesEndpoint).delete(id);
   return response;
 };
 
 export const addFriend = async (uid: string) => {
-  const response = await keepupApi.post(`${circlesEndpoint}/uid`, { user: uid });
+  const response = await keepupApiAxiosInstance(circlesEndpoint).post(uid, { user: uid });
   return response;
 };
 
 export const removeFriend = async (uid: string) => {
-  const response = await keepupApi.post(`${circlesEndpoint}/uid`, { user: uid });
+  const response = await keepupApiAxiosInstance(circlesEndpoint).post(uid, { user: uid });
   return response;
 };
