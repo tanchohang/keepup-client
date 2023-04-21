c9f86641eef74586df05e79018a96543fc066c1b
tanchohang <tanchohang@gmail.com>
2023-04-21 12:13:45 +0100

    feat: add refresh token to localstorage and use it in auth

--- src/components/require-auth.tsx
+++ src/components/require-auth.tsx
@@ -1,10 +1,12 @@
 import { Navigate, Outlet, useLocation } from 'react-router';
 import useAuth from '../context/auth.context';
+import useLocalStorage from '../hooks/useLocalstorage';
 
 const RequireAuth = () => {
   const { auth } = useAuth();
   const location = useLocation();
+  const refreshToken = localStorage.getItem('token');
 
-  return auth ? <Outlet /> : <Navigate to="/login" state={{ from: location }} replace />;
+  return auth && refreshToken ? <Outlet /> : <Navigate to="/login" state={{ from: location }} replace />;
 };
 export default RequireAuth;
