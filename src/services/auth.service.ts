a0dae4237678c96062e5a61d4142216d39128a55
tanchohang <tanchohang@gmail.com>
2023-04-21 14:33:22 +0100

    feat: add service function to call refresh token api

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,3 +4,14 @@ export const login = async (username: string, password: string) => {
   const response = await keepupApiAxiosInstance(authEndpoint).post('/login', { username, password });
   return response;
 };
+
+export const refreshAccessToken = async () => {
+  const response = await keepupApiAxiosInstance(authEndpoint).get('/refresh', {
+    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+  });
+  return response;
+};
+
+export const logout = async () => {
+  localStorage.removeItem('token');
+};
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -9,8 +9,55 @@ export const partiesEndpoint = 'parties';
 export const chatEndpoint = 'chats';
 export const messagesEndpoint = 'messages';
 
-export const keepupApiAxiosInstance = (endpoint: string) =>
-  axios.create({
-    baseURL: [baseAPIURL, endpoint].join('/'),
+export const keepupApiAxiosInstance = (endpoint: string) => {
+  const instance = axios.create({
+    baseURL: baseAPIURL,
     headers: { 'Content-Type': 'application/json' },
   });
+  instance.interceptors.request.use(
+    (config) => {
+      config.baseURL = [baseAPIURL, endpoint].join('/');
+      if (config.url !== '/login') {
+        const token = sessionStorage.getItem('accessToken');
+        if (token) {
+          config.headers['Authorization'] = 'Bearer ' + token; // for Spring Boot back-end
+          // config.headers['x-access-token'] = token; // for Node.js Express back-end
+        }
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  instance.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    async (err) => {
+      const originalConfig = err.config;
+
+      if (originalConfig.url !== '/login' && err.response) {
+        // Access Token was expired
+        if (err.response.status === 401 && !originalConfig._retry) {
+          originalConfig._retry = true;
+
+          try {
+            const rs = await instance.get('/auth/refresh', { headers: { Authorization: localStorage.getItem('token') } });
+
+            const { accessToken } = rs.data;
+            sessionStorage.setItem(accessToken, accessToken);
+
+            return instance(originalConfig);
+          } catch (_error) {
+            return Promise.reject(_error);
+          }
+        }
+      }
+
+      return Promise.reject(err);
+    }
+  );
+  return instance;
+};
