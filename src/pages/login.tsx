bef731afec11dfc584d1445af0e54c65df8e6b1f
tanchohang <tanchohang@gmail.com>
2023-04-22 13:08:48 +0100

    feat: display server sent login errors

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,6 +4,8 @@ import { FieldValues, useForm } from 'react-hook-form';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { login } from '../services/auth.service';
 import useAuth, { AuthUser as UserType } from '../context/auth.context';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
   const {
@@ -14,28 +16,33 @@ const Login = () => {
   const { setAuth } = useAuth();
   const navigate = useNavigate();
   const location = useLocation();
+  const [loginErrors, setLoginErrors] = useState<string[]>([]);
 
   async function submitHandler(data: FieldValues): Promise<void> {
     const { username, password } = data;
-    const { data: resData } = await login(username, password);
-    console.log(resData);
+    try {
+      const { data: resData } = await login(username, password);
+      if (resData) {
+        console.log(resData);
+        const user: UserType = {
+          id: resData.id,
+          email: resData.email,
+          username: resData.username,
+          fullname: resData.fullname,
+          accessToken: resData.accessToken,
+        };
 
-    if (resData) {
-      console.log(resData);
-      const user: UserType = {
-        id: resData.id,
-        email: resData.email,
-        username: resData.username,
-        fullname: resData.fullname,
-        accessToken: resData.accessToken,
-      };
+        setAuth(user);
+        localStorage.setItem('token', resData.refresh_token);
+        sessionStorage.setItem('accessToken', resData.access_token);
 
-      setAuth(user);
-      localStorage.setItem('token', resData.refresh_token);
-      sessionStorage.setItem('accessToken', resData.access_token);
-
-      if (location.state?.from) {
-        navigate(location.state?.from?.pathname || '/', { replace: true });
+        if (location.state?.from) {
+          navigate(location.state?.from?.pathname || '/', { replace: true });
+        }
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        Array.isArray(error.response?.data.message) ? setLoginErrors(error.response?.data.message) : setLoginErrors([error.response?.data.message]);
       }
     }
   }
@@ -43,10 +50,15 @@ const Login = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-cyan-500 text-base md:text-lg font-normal md:font-semibold">
       <form className="flex flex-col gap-8 w-[80%] md:w-[30%]" onSubmit={handleSubmit(submitHandler)}>
-        {Object.keys(errors).length > 0 && (
+        {(Object.keys(errors).length > 0 || loginErrors.length > 0) && (
           <div className="bg-red-200 text-red-600 p-4 ml-10 rounded-sm text-xs">
             <p>{errors.username && <span>Username is required</span>}</p>
             <p>{errors.password && <span>Password is required</span>}</p>
+            {loginErrors.map((e) => (
+              <p key={e}>
+                <span>{e}</span>
+              </p>
+            ))}
           </div>
         )}
         <label className="flex gap-5  items-center ">
