a32df4d4b8ca432b35865fb120bb6838bcad0628
tanchohang <tanchohang@gmail.com>
2023-04-22 12:24:29 +0100

    feat: check for refreshtoken to authorise unauth routes

--- src/components/unauthorised.tsx
+++ src/components/unauthorised.tsx
@@ -1,9 +1,28 @@
 import { Navigate, Outlet, useNavigate } from 'react-router';
-import useAuth from '../context/auth.context';
+import useAuth, { AuthUser } from '../context/auth.context';
+import { useEffect, useState } from 'react';
+import { refreshAccessToken } from '../services/auth.service';
 
 const UnAuthorized = () => {
-  const { auth } = useAuth();
+  const { auth, setAuth } = useAuth();
+  const refreshToken = localStorage.getItem('token');
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const getAccessToken = async () => {
+      try {
+        const { data } = await refreshAccessToken();
+        //   setAuth(userWithToken.data);
+        const { id, username, email, fullname, accessToken } = data;
+        setAuth({ id, username, email, fullname, accessToken } as AuthUser);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    !auth && refreshToken ? getAccessToken() : setIsLoading(false);
+  }, []);
 
-  return auth ? <Navigate to={`${auth.username}`} replace /> : <Outlet />;
+  return auth && refreshToken ? <Navigate to={`${auth?.username}`} replace /> : <Outlet />;
 };
 export default UnAuthorized;
