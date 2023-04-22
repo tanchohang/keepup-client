dd6525a115d50c05fbc39a01fb77352e74c8fedd
tanchohang <tanchohang@gmail.com>
2023-04-22 12:18:38 +0100

    feat: remove auth and navigate to "/" on logout

--- src/components/sidenav.tsx
+++ src/components/sidenav.tsx
@@ -1,12 +1,16 @@
 import { Calendar, MessageCircle, Newspaper, User, Users } from 'lucide-react';
 import { ComposedReactAppNavLink } from './nav-link';
 import { HTMLAttributes } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useAuth from '../context/auth.context';
 
 interface Props {
   className: string;
   hide: boolean;
 }
 function Sidenav({ className, hide }: Props) {
+  const navigate = useNavigate();
+  const { setAuth } = useAuth();
   return (
     // <div className="flex flex-col-reverse justify-between min-h-screen w-full">
     <nav className={['flex justify-around md:flex-col md:justify-normal md:gap-5 md:p-5 border-r border-zinc-100', className].join(' ')}>
@@ -19,6 +23,9 @@ function Sidenav({ className, hide }: Props) {
         onClick={() => {
           localStorage.clear();
           sessionStorage.clear();
+          setAuth(null);
+
+          navigate('/');
         }}
       >
         Logout
