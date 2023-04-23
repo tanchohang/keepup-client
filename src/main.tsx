72c8b82e1dbcacfee4bdd20645755e7b914da2d0
tanchohang <tanchohang@gmail.com>
2023-04-23 14:51:28 +0100

    feat: add appcontext provider to app component

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { AuthProvider } from './context/auth.context';
+import { AppContextProvider } from './context/app.context';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
     </AuthProvider>
   </React.StrictMode>
 );
