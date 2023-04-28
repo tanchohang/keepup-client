30ad1cf064edbf5a89bb8c77fa8b6019dbbaaf85
tanchohang <tanchohang@gmail.com>
2023-04-28 21:45:29 +0100

    feat: add logout button to mobile nav

--- src/components/chat/mobile-chat-sidenav.tsx
+++ src/components/chat/mobile-chat-sidenav.tsx
@@ -2,6 +2,7 @@ import useAuth from '../../context/auth.context';
 import { X } from 'lucide-react';
 import { useChatMiniContext } from '../../context/chat-mini.context';
 import { useAppContext } from '../../context/app.context';
+import { logout } from '../../services/auth.service';
 
 interface Props {}
 export const MobileChatSidenav = ({}: Props) => {
@@ -12,17 +13,31 @@ export const MobileChatSidenav = ({}: Props) => {
     <div
       className={`${showMobileSidebar ? 'flex' : 'hidden'} w-[250px] min-h-full flex-col gap-5 p-8 px-10 bg-emerald-800 relative text-white text-lg`}
     >
-      <button
-        onClick={() => {
-          setShowMobileSidebar(false);
-        }}
-      >
-        <X size={40} className="text-red-600 absolute right-0 top-0" />
-      </button>
-      <div>{auth?.fullname}</div>
-      <div>Requests</div>
-      <div>Accounts</div>
-      <div>Events</div>
+      <div>
+        <button
+          onClick={() => {
+            setShowMobileSidebar(false);
+          }}
+        >
+          <X size={40} className="text-red-600 absolute right-0 top-0" />
+        </button>
+        <div>{auth?.fullname}</div>
+        <div>Requests</div>
+        <div>Accounts</div>
+        <div>Events</div>
+      </div>
+
+      <div>
+        <button
+          className="bg-red-400"
+          onClick={async () => {
+            await logout();
+            window.location.reload();
+          }}
+        >
+          Logout
+        </button>
+      </div>
     </div>
   );
 };
